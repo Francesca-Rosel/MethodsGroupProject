43f25b08d6f339a6cb4a1ac564ebdf1500522df4
tducey03 <149282903+tducey03@users.noreply.github.com>
2023-11-09 11:48:32 -0600

    Zero Constructor plus User function

--- user.py
+++ user.py
@@ -1,7 +1,16 @@
 # user class. Tommy
 class user_class:
-  def placeholder:
-    print("This is a placeholder")
-
+  databaseName = ""
+  tableName = ""
+  userID = ""
+  loggedIn = False
+  def __init__(self, databaseName, tableName, userID, loggedIn):
+    self.databaseName = databaseName
+    self.tableName = tableName
+    self.userID = userID
+    self.loggedIn = loggedIn
+  def User(self, databaseName, tableName):
+    self.databaseName = databaseName
+    self. tableName = tableName
+  
 user = user_class()
-user. placeholder()
