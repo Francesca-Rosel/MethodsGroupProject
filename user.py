d2362ae9744c7c185e3922e339be578fd59e448f
tducey03 <149282903+tducey03@users.noreply.github.com>
2023-11-15 15:21:27 -0600

    Completed Login function

--- user.py
+++ user.py
@@ -2,16 +2,21 @@
 import sqlite3
 
 # connect to the database
-conn = sqlite3.connect('users.db')
+db = sqlite3.connect('users.db')
 
 # create a cursor object
-c = conn.cursor()
+c = db.cursor()
 
 # create a table
-c.execute('''CREATE TABLE userTable (UserID INT PRIMARY KEY NOT NULL, Email TEXT NOT NULL,
-            Password TEXT NOT NULL, FirstName TEXT NOT NULL, LastName TEXT NOT NULL,
-            Address TEXT NOT NULL, City TEXT NOT NULL, State TEXT NOT NULL, Zip INT NOT NULL,
-            Payment TEXT NOT NULL)''')
+c.execute('''CREATE TABLE userTable (UserID TEXT PRIMARY KEY NOT NULL, Email TEXT NOT NULL, Password TEXT NOT NULL, FirstName TEXT NOT NULL, LastName TEXT NOT NULL, Address TEXT NOT NULL, City TEXT NOT NULL, State TEXT NOT NULL, Zip INT NOT NULL, Payment TEXT NOT NULL)''')
+
+#populating with some random values from online generators
+#random values: FirstName, LastName, Address, City, State, Zip
+            #FirstName, LastName came from an online generator. Possible these are real people somewhere in the world
+            #Address, City, Zip are random values from an online generator given a random state. Possible these are real addresses
+c.execute('''INSERT INTO userTable (UserID, Email, Password, FirstName, LastName, Address, City, State, Zip, Payment) VALUES ('wf123', 'wfreeman1@gmail.com', 'HailState98!', 'Willie', 'Freeman', '2970 Froe St.', 'Paden City', 'West Virginia', '26159', 'PayPal')''')
+c.execute('''INSERT INTO userTable (UserID, Email, Password, FirstName, LastName, Address, City, State, Zip, Payment) VALUES ('fg456', 'fgarret2@gmail.com', 'MikeLeach23!', 'Freyja', 'Garret', '4567 Archwood Ave.', 'Newcastle', 'Wyoming', '82701', 'Visa')''')
+db.commit()
 
 class user_class:
 
@@ -26,6 +31,21 @@ class user_class:
     self.tableName = tableName
 
   def login(self):
+    num = 0
+              
+    attemptUser = input("Enter UserID: ")
+    attemptPassword = input("Enter Password: ")
+
+    c.execute('''SELECT UserID, Password FROM userTable''')
+
+    loginInfo = c.fetchall()
+    for id in loginInfo:
+        if ((attemptUser == loginInfo[num][0]) and (attemptPassword == loginInfo[num][1])):
+            self.userID = attemptUser
+            print("Logged In Successfully\n")
+            return True
+        num += 1
+    print("Login Failed. Check your UserID and Password! Try Again. \n")
     return False
 
   def logout(self):
