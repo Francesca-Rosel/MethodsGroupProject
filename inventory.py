6f9282de4ddb9a7acc4e4db351f529056a37ad35
Amelia M <149272012+agm475@users.noreply.github.com>
2023-11-16 13:57:39 -0600

    added viewInventory and updated the other functions in inventory.py

--- inventory.py
+++ inventory.py
@@ -3,8 +3,17 @@ import sqlite3
 
 db = sqlite3.connect("inventory.db")
 c = db.cursor()
-#database commands
-c.execute(''' CREATE TABLE inventory (ISBN TEXT NOT NULL,Title TEXT NOT NULL,Author TEXT NOT NULL,Genre TEXT NOT NULL,Pages TEXT NOT NULL,releaseData, TEXT NOT NULL,Stock TEXT NOT NULL,PRIMARY KEY (ISBN) )''')
+#create database
+c.execute(''' CREATE TABLE "inventory.db" (
+	"ISBN"	TEXT NOT NULL,
+	"Title"	TEXT NOT NULL,
+	"Author" TEXT NOT NULL,
+	"Genre"	TEXT NOT NULL,
+	"Pages"	TEXT NOT NULL,
+	"releaseData"	TEXT NOT NULL,
+	"Stock"	TEXT NOT NULL,
+	PRIMARY KEY("ISBN")
+);''')
 
 class Inventory:
     # initializes
@@ -16,25 +25,33 @@ class Inventory:
         self.database=database
         self.table=table
 
-    # getter - db name
-    def get_db_name(self):
-        return self.database
-
-    # getter - table name
-    def get_table_name(self):
-        return self.table
-
     # view inventory
-    def view_inventory(self, db_list):
-        for x in db_list:
-            print(x)
+    def view_inventory(self):
+        print("Inventory: \n")
+        print("---------")
+        c.execute("SELECT * FROM inventory")
+
+        rows = c.fetchall()
+        num = 1;
+        for row in rows:
+            print("Book " + num + ":\n")
+            print(row[0] + " | " + row[1] + " | " + row[2] + " | " + row[3] + " | " + row[4] + " | " + row[5] + " | " + row[6] + "\n")
+            num+=1
 
     # search inventory
-    def search_inventory(self, item):
-        self.db_name = item
-        print()
+    def search_inventory(self):
+        item = input("Enter the title of the item you'd like to find\n")
+        c.execute(("SELECT * FROM inventory WHERE Title=?", (item,)))
+        #finish this later
 
     # decrease stock
-    def decrease_stock(self, isbn):
+    def decrease_stock(self, isbn=""):
         self.isbn=isbn
         print()
+
+    # GETTERS
+    def get_db_name(self):
+        return self.database
+        
+    def get_table_name(self):
+        return self.table
