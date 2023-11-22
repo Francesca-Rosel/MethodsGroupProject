537aec541b9da55cdd7e5e535a82cd1c88a1fc62
Francesca Clavarino <142697184+Francesca-Rosel@users.noreply.github.com>
2023-11-22 12:47:04 -0600

    Update main.py
    I filled out more branched of main. Needs to be connected properly to classes.

--- main.py
+++ main.py
@@ -1,39 +1,64 @@
-#from user import *
-#from inventory import *
+from user import *
+from inventory import *
 #main function
-def logIn():
+def main(): 
   while True:
-    username = input("Please enter your username: ")
-    password = input("Please enter your password: ")
-    login = True
-    #if username and password in database then set to true. if not set login to false.
-    if (login == True):
-        return False 
-    #Direct user to logged in menu
-    elif(login == False):
-        print("incorrect username or password. Please try again.")
-        #back to menu
-        return False  
-
-      
-def main():
-  #outermenu = True
-  #innermenu = False
-  while True:
-    print("Welcome to The Online Bookstore! \nPlease select a valid option. \n1.Login \n2.Create Account \n3.Logout") 
+    print("Welcome to The Online Bookstore! \nPlease select a valid option. \n1.Login \n2.Create Account \n3.Exit") 
     option = input("Please choose a valid option: ")
 
     match option:
       case '1':
         print("login section")
-        logIn()
-        return False
+        #Logged In Main Menu
+        getLoggedIn()
+        if (loggedIn == True):
+          print("1. View Account Information \n2. Inventory Information \n3. Cart Information\n4. Logout")
+          option1 = input("Please choose a valid option:")
+          match option1:
+            #View Account Information: viewAccountInformation():
+            case '1': #connect to User class
+              break
+            #View Inventory Information
+            case '2': #connect to inventory class
+              print("1. View Inventory\n2.Search Inventory\n3. Go back")
+              option2 = input('')
+              match option2:
+                case '1': #viewInventory()
+                  break
+                case '2': #searchInventory()
+                  break
+                case '3':
+                  break
+            #View Cart Information
+            case '3': #connect to cart class
+              print("1. View Cart\n2. Add Items to Cart\n3. Remove an Item from Cart\n4. Check Out\n5. Go back")
+              option3 = input('')
+              match option3:
+                case '1': #viewCart()
+                  break
+                case '2': #addToCart()
+                  break
+                case '3': #removeFromCart()
+                  break
+                case '4': #checkOut()
+                  break
+                case '5':
+                  break
+            case '4':
+              break
+            case _:
+              print("Invalid option. Choose again.")
+        #Failed Log In Attempt
+        else:
+          print("Log In Attempt Unsuccessful.")
+          break
       case '2':
-        print("create account section")
-        return False
+        print("create account section") #createAccount()
       case '3':
-        print("Thank you for shopping with us! Good-bye")
+        print("Thank you for shopping with us! Good-bye") 
         return False
+      case _:
+        print("Invalid option. Choose again.")
   
 
 #this is calling the main function
