4c5e82711a296470d4b9c6d1617a732f9df173ef
Francesca Clavarino <142697184+Francesca-Rosel@users.noreply.github.com>
2023-11-29 14:15:13 -0600

    Update inventory.py
    AMELIA - updated her class with fixes from user.py

--- inventory.py
+++ inventory.py
@@ -4,7 +4,7 @@ import sqlite3
 db = sqlite3.connect("inventoryTable.db")
 c = db.cursor()
 #create database
-c.execute(''' CREATE TABLE "inventoryTable" (
+c.execute(''' CREATE TABLE IF NOT EXISTS inventoryTable (
 	"ISBN"	TEXT NOT NULL,
 	"Title"	TEXT NOT NULL,
 	"Author" TEXT NOT NULL,
@@ -16,11 +16,17 @@ c.execute(''' CREATE TABLE "inventoryTable" (
 ); ''')
 
 #add data
-c.execute('''INSERT INTO inventoryTable (ISBN, Title, Author, Genre, Pages, ReleaseDate, Stock) VALUES ('9781250891211', 'A Darker Shade of Magic', 'V.E Schwab', 'Fantasy', '416', '05/16/2023', '25')''')
-c.execute('''INSERT INTO inventoryTable (ISBN, Title, Author, Genre, Pages, ReleaseDate, Stock) VALUES ('9780062484390', 'And Then There Were None', 'Agatha Christie', 'Mystery', '256', '11/06/1939', '7') ''')
-c.execute('''INSERT INTO inventoryTable (ISBN, Title, Author, Genre, Pages, ReleaseDate, Stock) VALUES ('9780811204811', 'No Longer Human', 'Osamu Dazai', 'Fiction', '176', '01/17/1973', '15') ''')
-c.execute('''INSERT INTO inventoryTable (ISBN, Title, Author, Genre, Pages, ReleaseDate, Stock) VALUES ('9781716675522', 'Data Structures and Advanced Algorithms: Python', 'Rachel Xin Tony Lee Elisabeth Feng', 'Non-Fiction', '192', '08/07/2020', '5') ''')
-c.execute('''INSERT INTO inventoryTable (ISBN, Title, Author, Genre, Pages, ReleaseDate, Stock) VALUES ('9780141196886', 'Dracula', 'Bram Stoker', 'Horror', '512', '05/26/1897', '5') ''')
+
+try:
+    c.execute('''INSERT INTO inventoryTable (ISBN, Title, Author, Genre, Pages, ReleaseDate, Stock) VALUES ('9781250891211', 'A Darker Shade of Magic', 'V.E Schwab', 'Fantasy', '416', '05/16/2023', '25')''')
+    c.execute('''INSERT INTO inventoryTable (ISBN, Title, Author, Genre, Pages, ReleaseDate, Stock) VALUES ('9780062484390', 'And Then There Were None', 'Agatha Christie', 'Mystery', '256', '11/06/1939', '7') ''')
+    c.execute('''INSERT INTO inventoryTable (ISBN, Title, Author, Genre, Pages, ReleaseDate, Stock) VALUES ('9780811204811', 'No Longer Human', 'Osamu Dazai', 'Fiction', '176', '01/17/1973', '15') ''')
+    c.execute('''INSERT INTO inventoryTable (ISBN, Title, Author, Genre, Pages, ReleaseDate, Stock) VALUES ('9781716675522', 'Data Structures and Advanced Algorithms: Python', 'Rachel Xin Tony Lee Elisabeth Feng', 'Non-Fiction', '192', '08/07/2020', '5') ''')
+    c.execute('''INSERT INTO inventoryTable (ISBN, Title, Author, Genre, Pages, ReleaseDate, Stock) VALUES ('9780141196886', 'Dracula', 'Bram Stoker', 'Horror', '512', '05/26/1897', '5') ''')
+
+    db.commit()
+except sqlite3.IntegrityError:
+    print("")
 db.commit()
 
 class inventory_class:
