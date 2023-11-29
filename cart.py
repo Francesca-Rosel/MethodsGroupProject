4f1b22a24632e1f6c886dda0b96e80959cb02328
Francesca Clavarino <142697184+Francesca-Rosel@users.noreply.github.com>
2023-11-29 12:55:39 -0600

    Update cart.py

--- cart.py
+++ cart.py
@@ -1,1 +1,28 @@
-print("here we go")
+import sqlite3
+
+#connecting to the database
+db = sqlite3.connect("cartTable.db")
+c = db.cursor()
+
+#create database
+class Cart:
+
+    def __init__(self, databaseName="", tableName=""):
+        self.databaseName = databaseName
+        self.tableName = tableName
+
+    def Cart(self, databaseName, tableName):
+        self.databaseName = 'cartitems.db'
+        self.tableName = 'cartTable'
+    
+    def viewCart(userID, inventoryDatabase):
+        return None
+        
+    def addToCart(userID, ISBN):
+        return None
+    
+    def removeFromCart(userID, ISBN):
+        return None
+    
+    def checkOut(userID):
+        return None
