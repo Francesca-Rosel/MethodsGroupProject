abcd8bdc3ac85434e129fc4d4a80b7309a3b0c12
Francesca Clavarino <142697184+Francesca-Rosel@users.noreply.github.com>
2023-11-29 22:24:03 -0600

    Update cart.py
    Troubleshooting being able to use the inventoryTable inside of my class. Difficulty: Impossible

--- cart.py
+++ cart.py
@@ -1,14 +1,14 @@
 #Cart Class - Francesca
 
 import sqlite3
-import inventory
+import inventory 
 
 #connecting to the database
 db = sqlite3.connect("cartTable.db")
 c = db.cursor()
 
 #create database
-c.execute('''CREATE TABLE IF NOT EXISTS userTable (UserID TEXT NOT NULL, ISBN TEXT NOT NULL, Quantity TEXT NOT NULL, FOREIGN KEY (UserID) REFERENCES userTable(UserID), FOREIGN KEY (ISBN) REFERENCES inventoryTable(ISBN))''')
+c.execute('''CREATE TABLE IF NOT EXISTS cartTable (UserID TEXT NOT NULL, ISBN TEXT NOT NULL, Quantity TEXT NOT NULL, FOREIGN KEY (UserID) REFERENCES userTable(UserID), FOREIGN KEY (ISBN) REFERENCES inventoryTable(ISBN))''')
 
 db.commit()
 
@@ -22,7 +22,7 @@ class cart_class:
         self.databaseName = 'cartitems.db'
         self.tableName = 'cartTable'
     
-    def viewCart(self, userID, inventoryTable):
+    def viewCart(self, userID, inventoryDatabase):
         print("Shopping Cart:\n")
         c.execute("SELECT * FROM inventoryTable INNER JOIN cartTable ON inventoryTable.ISBN = cartTable.ISBN AND UserID =?", (userID,))
 
@@ -36,19 +36,13 @@ class cart_class:
         
         
     def addToCart(self, userID, ISBN):
-
         desiredQuantity = input("Number of copies: ")
-        '''c.execute("SELECT Stock FROM inventoryTable WHERE ISBN =?", (attemptedISBN,))
-        available = c.fetchone()
-        while (desiredQuantity > int(available[0])):
-            print("Error. Not Enough Stock. Please choose less than ", available)
-            desiredQuantity = input("Number of copies: ")
-        '''
         c.execute("INSERT INTO cartTable VALUES(?, ?, ?)", (userID, ISBN, desiredQuantity))
         
         
     
     def removeFromCart(self, userID, ISBN):
+        c.execute("DELETE FROM cartTable WHERE UserID =? AND ISBN =?", (userID, ISBN))
         return None
     
     def checkOut(self, userID):
@@ -59,6 +53,6 @@ class cart_class:
         for i in range(cartInfo[num][2]):
             blackberry.decrease_stock(cartInfo[num][1])
             num += 1
-        c.execute("TRUNCATE TABLE cartTable")
+        c.execute("DELETE FROM Customer WHERE UserID =?", (userID,))
 
         
